1e06aef0970f473578561f4c80f5cdcd9e84400e
CarloDettori <dettoric07@gmail.com>
2025-01-31 18:30:54 +0100

    travel dettail page

--- src/components/pages/TravelDetailsPage.jsx
+++ src/components/pages/TravelDetailsPage.jsx
@@ -1,11 +1,29 @@
-export default function TravelDetailsPage({ trip }) {
+import { trips, guides, clients } from "../../models/db_trips.js"
+import TravelComponent from "../common/TravelComponent"
+import ClientNamSurComponent from "../common/clientNamSurComponent.jsx"
+
+
+export default function TravelDetailsPage({ trip, index }) {
+
+    let tripClients = []
+
+    if (clients.data.tripId === index) {
+        tripClients.push(clients[index])
+    } else { }
+
     return (
         <main>
-            <div>
-                <h2>{trip}</h2>
+            <div className="one-trip">
+                <TravelComponent tripData={trip[index]} />
             </div>
-            <div>
 
+            <div>
+                <h2>Rubrica Partecipanti</h2>
+                {tripClients.map((client) => {
+                    return (
+                        <ClientNamSurComponent key={client.id} />
+                    )
+                })}
             </div>
         </main>
     )
