a211e490970d428c30824cdb662c5d07a1f963c7
CarloDettori <dettoric07@gmail.com>
2025-01-31 17:38:36 +0100

    rooting

--- src/components/pages/TravelListPage.jsx
+++ src/components/pages/TravelListPage.jsx
@@ -5,9 +5,9 @@ import { trips, guides, clients } from "../../models/db_trips"
 export default function TravelListPage() {
     return (
         <main>
-            {trips.map((trip) => {
+            {/* {trips.map((trip) => {
                 return <TravelComponent key={trip.id} tripData={trip} />
-            })}
+            })} */}
         </main>
     )
 }
