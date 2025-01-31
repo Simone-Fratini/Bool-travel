b2d2ef94cc0b7c1d520bc0c88ff965309d994573
MicheleSaraceni <michele.saraceni23@email.com>
2025-01-31 17:34:09 +0100

    fixed error 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Router, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TravelListPage from "./components/pages/TravelListPage.jsx"
 import TravelDetailsPage from "./components/pages/TravelDetailsPage.jsx"
 import DefaultLayout from "./components/layout/DefaultLayout.jsx"
@@ -6,14 +6,14 @@ import DefaultLayout from "./components/layout/DefaultLayout.jsx"
 export default function App() {
     return (
         <BrowserRouter>
-            <Router>
+            <Routes>
                 <Route element={<DefaultLayout />}>
                     <Route path="/">
                         <Route index element={<TravelListPage />} />
                         <Route path=":id" element={<TravelDetailsPage />} />
                     </Route>
                 </Route>
-            </Router>
+            </Routes>
         </BrowserRouter>
     );
 }
