c7a86e08a6afb78cc2ba8b6222220308cddb31cc
MicheleSaraceni <michele.saraceni23@email.com>
2025-01-31 16:40:50 +0100

    Routering

---
+++ src/components/pages/TravelListPage.jsx
@@ -0,0 +1,9 @@
+<h1 className="w-full py-4 text-6xl text-center">
+                    My React Template
+                </h1>
+                <h2 className="py-4 text-3xl text-center">Packages installed:</h2>
+                <ul className="text-center">
+                    <li>Tailwind css</li>
+                    <li>react-router-dom</li>
+                    <li>axios</li>
+                </ul>
