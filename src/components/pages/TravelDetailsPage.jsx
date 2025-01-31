b7cbc4e8f5899e72c0f47070d6b6d42d87389812
CarloDettori <dettoric07@gmail.com>
2025-01-31 17:33:20 +0100

    aadding id to travel component

--- src/components/pages/TravelDetailsPage.jsx
+++ src/components/pages/TravelDetailsPage.jsx
@@ -1,3 +1,12 @@
 export default function TravelDetailsPage({ trip }) {
-    return <h1>TRAVEL DETTAILS</h1>
+    return (
+        <main>
+            <div>
+                <h2>{trip}</h2>
+            </div>
+            <div>
+
+            </div>
+        </main>
+    )
 }
