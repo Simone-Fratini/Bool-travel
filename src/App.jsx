25c8f0498afd94bec4f0447dd3a22af3b6c6f4e9
Simone-Fratini <simone@fratini.com>
2025-02-03 13:10:43 +0100

    remove unused ClientDetailsComponent route from App

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TravelListPage from "./components/pages/TravelListPage.jsx"
 import TravelDetailsPage from "./components/pages/TravelDetailsPage.jsx"
 import DefaultLayout from "./components/layout/DefaultLayout.jsx"
-import ClientDetailsComponent from "./components/common/ClientDetailscomponent.jsx";
 //import { trips, clients, guides } from "./models/db_trips.js";
 
 export default function App() {
@@ -14,7 +13,6 @@ export default function App() {
                     {/* index = path="/" */}
                     <Route index element={<TravelListPage />} />
                     <Route path=":travelId/" element={<TravelDetailsPage />} />
-                    <Route path="client/:clientId" element={<ClientDetailsComponent />} />
                 </Route>
             </Routes>
         </BrowserRouter>
