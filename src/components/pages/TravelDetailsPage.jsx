623aba7fc4bc117d5b2a7d884a47d090b28ef3fd
CarloDettori <dettoric07@gmail.com>
2025-02-03 16:36:36 +0100

    fixing attempt to search barr

--- src/components/pages/TravelDetailsPage.jsx
+++ src/components/pages/TravelDetailsPage.jsx
@@ -14,6 +14,8 @@ export default function TravelDetailsPage() {
     //console.log(trips[travelId])
     //creo un avariabile di stato per tenere traccia del valore (stringa) scritto nella search bar
     const [searchImputValue, setSearchImputValue] = useState("")
+    const [searchedClients, setSearchedClients] = useState([])
+    const [fakeTripClients, setFakeTripClients] = useState([])
 
     // creo la variabile tripClients e le assegno una array vuota
 
@@ -24,10 +26,13 @@ export default function TravelDetailsPage() {
 
         return (client.tripId === trips[travelId].id)
     })
+    setFakeTripClients([...tripClients])
+
     //console.log(tripClients)
     //ora che abbiamo solo i clienti partecipanti al viaggio implementiamo un altro filtraggio che seleziona i clienti in base al valore scritto nella search bar. quest'ulteriore filtraggio returnerà un'arrei contentente soltanto i clienti che nel loro nome o cognome è incluso il valore inserito nella searchbar
     function clientsSearchFilter() {
         if (searchImputValue === "") {
+            setSearchedClients(fakeTripClients)
         } else {
             console.log(searchImputValue)
             const tripSearchedClients = tripClients.filter((client) =>
@@ -36,7 +41,8 @@ export default function TravelDetailsPage() {
 
             );
             console.log(tripSearchedClients)
-            setSearchImputValue(tripSearchedClients)
+            setSearchedClients(tripSearchedClients)
+            console.log(searchedClients)
         }
         //client.toLowerCase(client.firstName).includes(toLowerCase(searchValue)) ||
         //client.toLowerCase(client.lastName).includes(toLowerCase(searchValue))
@@ -52,7 +58,7 @@ export default function TravelDetailsPage() {
     //assegnamo alla variabile di stato il valore definito nella searchbar, cosi ogni volta che il valore della serarchbar cambia avremo un nuovo rendering dei partecipanti ad un viaggio, in questo modo non c'è bisogno di un button per confermare il valore scritto nella searcbar
     function handleChange(event) {
         setSearchImputValue(event.target.value);
-        //console.log(searchImputValue)
+        console.log(searchImputValue)
     }
 
 
@@ -60,7 +66,7 @@ export default function TravelDetailsPage() {
         <div className="container mx-auto p-4 ">
             <div>
                 {/* [travelId - 1] per sincronizzare id e index, che poi inserendo il database useremo direttamente l'index */}
-                <TravelDetailComponent tripData={trips[travelId - 1]} guidesData={guides}/>
+                <TravelDetailComponent tripData={trips[travelId - 1]} guidesData={guides} />
             </div>
             <div>
                 <h2>Rubrica Partecipanti</h2>
@@ -78,12 +84,12 @@ export default function TravelDetailsPage() {
                             className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             placeholder="Search Mockups, Logos..."
                             value={searchImputValue}
-                            onChange={handleChange}
+                            onChange={(e) => { setSearchImputValue(e.target.value) }}
                             required
                         />
                     </div>
                 </form>
-                {tripClients.map((client) => {
+                {searchedClients.map((client) => {
                     return (
                         <div key={client.id} >
                             <ClientNamSurComponent key={client.id} clientData={client} />
